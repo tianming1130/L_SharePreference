f3d0cc34c05948cee0d3f51f727c6a22d9397039
zhengtianming <491231941@qq.com>
2017-11-27 21:49:11 +0800

    修改

--- app/src/main/java/zknu/cn/l_sharepreference/MainActivity.java
+++ app/src/main/java/zknu/cn/l_sharepreference/MainActivity.java
@@ -5,36 +5,46 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private static final String USER_INFO = "user_info";
     private EditText etUserName,etPassword;
-    private Button btnAdd,btnDel,btnClear;
-    private int number;
+    private Button btnLogin,btnClear;
+    private CheckBox cbSaveInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etUserName=(EditText)findViewById(R.id.et_user_name);
         etPassword=(EditText)findViewById(R.id.et_password);
-        btnAdd=(Button)findViewById(R.id.btn_add);
-        btnDel=(Button)findViewById(R.id.btn_del);
+        cbSaveInfo=(CheckBox)findViewById(R.id.cb_save_info);
+        btnLogin =(Button)findViewById(R.id.btn_login);
         btnClear=(Button)findViewById(R.id.btn_clear);
 
-        btnAdd.setOnClickListener(this);
-        btnDel.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
         btnClear.setOnClickListener(this);
 
+        init();
+
     }
+
+    private void init() {
+        SharedPreferences sp=getSharedPreferences(USER_INFO,MODE_PRIVATE);
+        String strUserName=sp.getString("USER","");
+        String strPassword=sp.getString("PASSWORD","");
+        etUserName.setText(strUserName);
+        etPassword.setText(strPassword);
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_add:
-                add();
-                break;
-            case  R.id.btn_del:
-                del();
+            case R.id.btn_login:
+                login();
                 break;
             case R.id.btn_clear:
                 clear();
@@ -43,31 +53,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void clear() {
-        SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
+        SharedPreferences sp = getSharedPreferences(USER_INFO, MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.clear();
         editor.commit();
     }
 
-    private void del() {
-        SharedPreferences sp = getSharedPreferences("data", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        String strUserNameKey = etUserName.getText().toString();
-        String strPasswordKey = etPassword.getText().toString();
+    private void login() {
+        if (cbSaveInfo.isChecked()){
+            String strUserName=etUserName.getText().toString();
+            String strPassword=etPassword.getText().toString();
+            SharedPreferences sp=getSharedPreferences(USER_INFO,MODE_PRIVATE);
+            SharedPreferences.Editor editor=sp.edit();
+            editor.putString("USER",strUserName);
+            editor.putString("PASSWORD",strPassword);
+            editor.commit();
 
-        editor.remove(strUserNameKey);
-        editor.remove(strPasswordKey);
-        editor.commit();
-    }
+            Toast.makeText(this,"用户信息已经保存！",Toast.LENGTH_LONG).show();
 
-    private void add() {
-        String strUserName=etUserName.getText().toString();
-        String strPassword=etPassword.getText().toString();
-        SharedPreferences sp=getSharedPreferences("data",MODE_PRIVATE);
-        SharedPreferences.Editor editor=sp.edit();
-        number++;
-        editor.putString("USER"+String.valueOf(number),strUserName);
-        editor.putString("PASSWORD"+String.valueOf(number),strPassword);
-        editor.commit();
+        }else {
+            Toast.makeText(this,"未保存用户信息！",Toast.LENGTH_LONG).show();
+        }
     }
 }
